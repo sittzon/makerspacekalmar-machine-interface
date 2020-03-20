41b3467aac4c54b1af150467f2d120db596a840a
Magnus Edetorn <magnus.edetorn@tieto.com>
2020-03-20 11:34:08 +0100

    readTag function

--- gpio.js
+++ gpio.js
@@ -29,9 +29,38 @@ var updateDbUserTagIds = function() {
 	console.log('MOCK: Get user tag IDs from DB');
 }
 
-//Read tag and set authoried variable if tag is authorized
+//Read tag and set authorized variable for 10s if tag is authorized
 var readTag = function () {
 	// console.log('MOCK: Read tag');
+	rpio.spiBegin();
+	rpio.spiChipSelect(0);                  /* Use CE0 */
+	rpio.spiSetCSPolarity(0, rpio.HIGH);    /* AT93C46 chip select is active-high */
+	rpio.spiSetClockDivider(128);           /* AT93C46 max is 2MHz, 128 == 1.95MHz */
+	rpio.spiSetDataMode(0);
+	/*
+	 * There are various magic numbers below.  A quick overview:
+	 *
+	 *   tx[0] is always 0x3, the EEPROM READ instruction.
+	 *   tx[1] is set to var i which is the EEPROM address to read from.
+	 *   tx[2] and tx[3] can be anything, at this point we are only interested in
+	 *     reading the data back from the EEPROM into our rx buffer.
+	 *
+	 * Once we have the data returned in rx, we have to do some bit shifting to
+	 * get the result in the format we want, as the data is not 8-bit aligned.
+ 	*/
+
+	var tx = new Buffer([0x3, 0x0, 0x00, 0x00]);
+	var rx = new Buffer(4);
+	var out;
+	var i, j = 0;
+
+	for (i = 0; i < 128; i++, ++j) {
+        tx[1] = i;
+        rpio.spiTransfer(tx, rx, 4);
+        out = ((rx[2] << 1) | (rx[3] >> 7));
+        // console.log(out.toString(16) + ((j % 16 == 0) ? '\n' : ' '));
+	}
+	rpio.spiEnd();
 }
 
 //Returns if read tag id is authorized
@@ -46,8 +75,8 @@ var startMachine = function(machineId) {
 
 	//Set pin to high
 	//TODO
-	// rpio.open(pinsPhysical[pinNr], rpio.OUTPUT);
-	// rpio.write(pinsPhysical[machineId], rpio.HIGH);
+	rpio.open(pinsPhysical[machineId], rpio.OUTPUT);
+	rpio.write(pinsPhysical[machineId], rpio.HIGH);
 
 	//Set timer to 60s
 	pinsTimeLeft[machineId] = 60;
@@ -57,7 +86,6 @@ var startMachine = function(machineId) {
 setInterval(function(){
 	updateDbUserTagIds();
 }, 60000);
-
 
 //Update user tag Id's when app starts
 updateDbUserTagIds();
